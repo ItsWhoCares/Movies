fd320c3a19bd2dcfdde6c85b30508513dbdce4ef
ItsWhoCares <itswhocaresman@gmail.com>
2022-06-18 21:25:25 +0530

    First Prototype

---
+++ helpers.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import urllib.parse
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def lookup(q):
+    """Look up quote for symbol."""
+
+    # Contact API
+    try:
+        api_key = "k_76mo396f"
+        url = f"https://imdb-api.com/en/API/SearchMovie/{api_key}/{q}"
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    # Parse response
+    try:
+        result = response.json()
+        return result
+    except (KeyError, TypeError, ValueError):
+        return None
+
